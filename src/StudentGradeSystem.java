40f6b9b279eb31d70f95f276cdf60aca49f183c5
Imesh <imesh00senavirathna@gmail.com>
2026-06-10 21:18:47 +0530

    Add student input.

--- src/StudentGradeSystem.java
+++ src/StudentGradeSystem.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StudentGradeSystem {
+
+    static ArrayList<Student> studentList = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
+    public static void addStudent() {
+
+        System.out.print("Student ID: ");
+        String id = sc.nextLine();
+
+        System.out.print("Student Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Marks: ");
+        double marks = sc.nextDouble();
+        sc.nextLine();
+
+        studentList.add(new Student(id, name, marks));
+
+        System.out.println("Record Added.");
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Student Grade Management System");
+
+        addStudent();
     }
 }
