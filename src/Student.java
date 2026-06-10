3bbcd732c0a855737396ca4f915b92f3d30a740e
Imesh <imesh00senavirathna@gmail.com>
2026-06-10 21:16:40 +0530

    Create Student.java

---
+++ src/Student.java
@@ -0,0 +1,31 @@
+public class Student {
+
+    private String id;
+    private String name;
+    private double marks;
+
+    public Student(String id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+               ", Name: " + name +
+               ", Marks: " + marks;
+    }
+}
